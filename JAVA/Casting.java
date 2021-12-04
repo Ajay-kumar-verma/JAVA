c80e1efca5c4e728557b77d70ff453018c15b1d4
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2021-12-04 23:56:41 +0530

    JAVA PROGRAM

--- JAVA/Casting.java
+++ JAVA/Casting.java
@@ -18,11 +18,21 @@ byte c1='A';
 // int  c1='A';
 System.out.println(c1);
 
-char c2=65;
+char c2=65; // while compile time it gets converted into an char
 System.out.println(c2);
 
 // In char and int no need to specify the type
 // it auto gets converted ....
+
+// While compile time it possible Run time it not possible 
+
+// int x2=65;
+// char c4=x2;
+
+
+char c4='a';
+int c5=c4;
+System.out.println(c5);
 
 
 
